9476e483afeb40954b754215a030554d6bb9e071
Prince Codes <princecodes247@gmail.com>
2024-07-31 22:27:26 +0100

    init

---
+++ app/layout.config.tsx
@@ -0,0 +1,44 @@
+import { type DocsLayoutProps } from 'fumadocs-ui/layout';
+import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
+import { pageTree } from '@/app/source';
+
+// shared configuration
+export const baseOptions: HomeLayoutProps = {
+  nav: {
+    title: 'Monarch ORM',
+  },
+  links: [
+    {
+      text: 'Documentation',
+      url: '/docs',
+      active: 'nested-url',
+    },
+  ],
+};
+
+// docs layout configuration
+export const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: pageTree,
+  sidebar: {
+    banner: (
+      <RootToggle
+        options={[
+          {
+            icon: <></>,
+            title: 'Core Docs',
+            description: 'Documentation for Monarch ORM',
+            url: '/docs',
+          },
+          {
+            icon: <></>,
+            title: 'Folder 2',
+            description: 'Pages in folder 2',
+            url: '/docs/studio',
+          },
+        ]}
+      />
+    ),
+  }
+};
